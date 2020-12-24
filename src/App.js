bad8875049853d5ff77bbaf09a49634b4590e419
collin-smith <74462915+collin-smith@users.noreply.github.com>
2020-12-24 13:49:39 -0700

    checking in at Dec 24 1:49pm

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This was created by Collin Smith changed at Dec 24 1:48pm
         </p>
         <a
           className="App-link"
